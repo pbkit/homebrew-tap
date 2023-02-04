0ce49d7bae9cdcf001fd20cd88db7e5a8bfac74e
pbkit <jong@chan.moe>
2023-02-04 05:45:58 +0000

    Bump pbkit to v0.0.61

--- Formula/pbkit.rb
+++ Formula/pbkit.rb
@@ -1,8 +1,8 @@
 class Pbkit < Formula
   desc "Protobuf toolings"
   homepage "https://github.com/pbkit/pbkit"
-  url "https://api.github.com/repos/pbkit/pbkit/tarball/v0.0.60"
-  sha256 "69d918a51fa0f249696d6ac702f6a029376abe074214365e727958c4985cb20d"
+  url "https://api.github.com/repos/pbkit/pbkit/tarball/v0.0.61"
+  sha256 "d286f9d41ff7152c8c1fd2004cc7705cc0c1044a60c24753f0f95b6236963b63"
   license any_of: ["Apache-2.0", "MIT"]
   depends_on "deno"
   def install
